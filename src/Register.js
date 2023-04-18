8b7f3ace0585011257b28688861bfef9b81462c7
Emerson Lewiecki <elewiecki1@verizon.net>
2023-04-18 13:17:04 -0400

    added register page with pass confirmation

--- src/Register.js
+++ src/Register.js
@@ -1,12 +1,79 @@
-import { Link } from "react-router-dom";
+import { useState } from "react"
+import { TextField, Stack, Button } from "@mui/material"
+import Service from "./Service"
+import { Link } from "react-router-dom"
 
 
 export default function Register(){
 
+    let [userName, setUserName] = useState("")
+    let [password, setPassword] = useState("")
+    let [confirmPassword, setConfirmPassword] = useState("")
+    let [errorMsg, setErrorMsg] = useState("")
+
+    function attemptRegister(){
+        if(password !== confirmPassword){
+            setErrorMsg("Password does not match")
+            return
+        }
+        let user = {userName, password}
+        Service.registerUser(user)
+        setErrorMsg("WHO'S MORE FOOLISH, THE FOOL, OR THE FOOL WHO FOLLOWS HIM")
+    }
+
+
     return(
-        <Link to={"/"}>
-            Back to Calendar (placeholder)
-        </Link>
+    <div style={{textAlign:"center"}}>
+
+        <h2>Create an Account</h2>
+
+        <div>
+        <TextField
+            id="username-input"
+            label="Username"
+            type="username"
+            autoComplete="current-username"
+            variant="standard"
+            value={userName}
+            onChange={e=>setUserName(e.target.value)}
+        />
+        </div>
+        <div>
+        <TextField
+            id="password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="standard"
+            value={password}
+            onChange={e=>setPassword(e.target.value)}
+        />
+        </div>
+        <div>
+        <TextField
+            id="confirm-password-input"
+            label="Confirm Password"
+            type="password"
+            autoComplete="current-password"
+            variant="standard"
+            value={confirmPassword}
+            onChange={e=>{setConfirmPassword(e.target.value)}}
+        />
+        </div>
+
+        <div style={{color: "red"}}>{password === confirmPassword? <br></br> : "Password does not match"}</div>
+
+        <div>
+        <Button onClick={attemptRegister}>Register</Button>
+        </div>
+
+        <div style={{color:"red"}}>{errorMsg}</div>
+
+        <div style={{paddingTop:"5px"}}>
+        <Link to={"/"}>Return to Home</Link>
+        </div>
+
+    </div>
     )
 
 }
