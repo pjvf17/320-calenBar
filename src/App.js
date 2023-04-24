84464527bd960dea61a1e094c3690eedfa3a837d
James Leone <jamesleone@Jamess-MacBook-Pro.local>
2023-04-24 18:17:12 -0400

    Add theme capability to App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CalendarPage from "./CalendarPage";
 import Register from "./Register"
 
+import { ThemeProvider, createTheme } from "@mui/material";
+import "./styles.css";
+
 export const ReloadCalendarContext = createContext({
   reloadCalendar: false,
   setReloadCalendar: () => {},
