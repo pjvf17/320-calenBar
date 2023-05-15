b479c1afaa1f69eba2043080d9f8c90e788e8dca
James Leone <jleone@umass.edu>
2023-05-15 16:31:05 -0400

    Add styling to login and register buttons using a paper component and mui colors, on hover features

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,17 +1,22 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import CalendarPicker from './CalendarPicker';
-import AddCalendar from './AddCalendar';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import CalendarPicker from "./CalendarPicker";
+import AddCalendar from "./AddCalendar";
 import { Link } from "react-router-dom";
-import { AccountCircle } from '@mui/icons-material';
+import { AccountCircle, HowToReg } from "@mui/icons-material";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import LoginIcon from "@mui/icons-material/Login";
+import Login from "./Login";
+import Divider from "@mui/material/Divider";
+import Paper from "@mui/material/Paper";
 
 function ResponsiveAppBar(props) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -33,11 +38,9 @@ function ResponsiveAppBar(props) {
   };
 
   return (
-    <AppBar position="fixed" style={{ background: '#2E3B55' }}>
+    <AppBar position="fixed" style={{ background: "#2E3B55" }}>
       <Container maxWidth="xl">
-
         <Toolbar disableGutters>
-
           <Typography
             variant="h6"
             noWrap
@@ -45,60 +48,92 @@ function ResponsiveAppBar(props) {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             calenBar
           </Typography>
 
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex'} }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             <CalendarPicker
-                calendar={props.calendar}
-                setCalendar={props.setCalendar}
+              calendar={props.calendar}
+              setCalendar={props.setCalendar}
             ></CalendarPicker>
             <AddCalendar></AddCalendar>
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-          
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <AccountCircle  sx={{color:"white"}}></AccountCircle>
+                <AccountCircle sx={{ color: "white" }}></AccountCircle>
               </IconButton>
             </Tooltip>
-
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-
-                <MenuItem onClick={handleCloseUserMenu}>
-                    <Link to={"/login"} style={{paddingRight:"10px"}}>Login</Link>
+            <Paper>
+              <Menu
+                sx={{
+                  mt: "45px",
+                  "& .MuiMenu-paper": { backgroundColor: "#232629" },
+                }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem
+                  onClick={handleCloseUserMenu}
+                  sx={{ "&:hover": {
+                    backgroundColor: "#424242",
+                  },}}
+                >
+                  <LoginIcon fontSize="small" sx={{ color: "white" }} />
+                  <Link
+                    to={"/login"}
+                    style={{
+                      marginLeft: "10px",
+                      color: "white",
+                      textDecoration: "none",
+                      fontFamily: "Merriweather",
+                    }}
+                  >
+                    Login
+                  </Link>
                 </MenuItem>
-                <MenuItem onClick={handleCloseUserMenu}>
-                    <Link to={"/register"}>Register</Link>  
+                <Divider sx={{ my: 0.5, backgroundColor: "#3D3D3D", marginLeft: "0.5em", marginRight: "0.5em" }} />
+                <MenuItem
+                  onClick={handleCloseUserMenu}
+                  sx={{ "&:hover": {
+                    backgroundColor: "#424242",
+                  },}}
+                >
+                  <HowToReg sx={{ color: "#FFF" }} />
+                  <Link
+                    to={"/register"}
+                    style={{
+                      marginLeft: "5px",
+                      color: "white",
+                      textDecoration: "none",
+                      fontFamily: "Merriweather",
+                    }}
+                  >
+                    Register
+                  </Link>
                 </MenuItem>
-
-            </Menu>
-
+              </Menu>
+            </Paper>
           </Box>
         </Toolbar>
       </Container>
